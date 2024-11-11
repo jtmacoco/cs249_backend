da56c935c130d467c91837e5c1d54397b6e71d3f
Joanthan Macoco <jtmacoco@gmail.com>
2024-11-10 21:47:52 -0800

    added simple sockets to brodcast changes to simple document

--- server.js
+++ server.js
@@ -10,24 +10,32 @@ const user = process.env.UNAME
 const server = http.createServer(app)
 const io = Server(server, {
     cors: {
-        origin: "*", 
+        origin: "*",
         methods: ["GET", "POST"]
     }
 });
 
 const uri = `mongodb+srv://${user}:${db_pass}@cluster0.t4uwd.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`
 const clientOptions = { serverApi: { version: '1', strict: true, deprecationErrors: true } };
+let documentContent = ""
 async function connectDB() {
-    try{
-        await mongoose.connect(uri,clientOptions)
+    try {
+        await mongoose.connect(uri, clientOptions)
         console.log("success connected to db")
     }
-    catch(error){
+    catch (error) {
         console.log(error)
     }
 }
-io.on('connection',(socket)=>{
+io.on('connection', (socket) => {
     console.log('a user connected')
+    socket.emit('documentUpdate', documentContent);
+
+    socket.on('documentUpdate', (content) => {
+        documentContent = content
+        io.emit('documentUpdate', documentContent)
+    })
+
 })
 connectDB()
-server.listen(PORT,() => {console.log(`starting server using port ${PORT}`)})
+server.listen(PORT, () => { console.log(`starting server using port ${PORT}`) })
