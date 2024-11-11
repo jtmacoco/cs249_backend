fa41c3b71ad66a06f71639f0fdb0b95362c41a4e
EC2 Default User <ec2-user@ip-172-31-30-91.ec2.internal>
2024-11-11 03:21:43 +0000

    working on sockets

--- server.js
+++ server.js
@@ -1,10 +1,20 @@
 require('dotenv').config();
 const express = require("express");
+const Server = require("socket.io")
+const http = require("http")
 const mongoose = require("mongoose");
 const app = express();
 const PORT = process.env.PORT || 3000;
 const db_pass = process.env.DB_PASS
 const user = process.env.UNAME
+const server = http.createServer(app)
+const io = Server(server, {
+    cors: {
+        origin: "*", 
+        methods: ["GET", "POST"]
+    }
+});
+
 const uri = `mongodb+srv://${user}:${db_pass}@cluster0.t4uwd.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`
 const clientOptions = { serverApi: { version: '1', strict: true, deprecationErrors: true } };
 async function connectDB() {
@@ -16,5 +26,8 @@ async function connectDB() {
         console.log(error)
     }
 }
+io.on('connection',(socket)=>{
+    console.log('a user connected')
+})
 connectDB()
-app.listen(PORT,() => {console.log(`starting server using port ${PORT}`)})
+server.listen(PORT,() => {console.log(`starting server using port ${PORT}`)})
