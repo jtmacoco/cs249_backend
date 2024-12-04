06832d0385dee6a79b620a88c73362c62cdec5fa
jonathan <jtmacoco@mail.csuchico.edu>
2024-12-04 12:44:18 -0800

    changed ip

--- Endpoints.js
+++ Endpoints.js
@@ -1,7 +1,5 @@
 class EndPoint {
-    static baseUrl = "http://192.168.1.142:8000/api/";
-
-    //static baseUrl = "http://localhost:8000/api/";
+    static baseUrl = "http://localhost:8000/api/";
     static updateDocument= '/document/update-document'
      static getDocument = '/document/get-document'
     static getFullUrl(endpoint) {
